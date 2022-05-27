8f391c1c1ee647fd8a431156c0329ba5ab8696ec
Jason Xie <thejasonxie@gmail.com>
2022-05-27 00:01:40 -0400

    Refactored actions to get userand repo

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,7 +5,7 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
-import { getUser, getUserRepos } from '../context/github/GitHubActions'
+import { getUserAndRepos } from '../context/github/GitHubActions'
 
 function User() {
     const { user, loading, repos, dispatch }= useContext(GitHubContext)
@@ -16,16 +16,10 @@ function User() {
         dispatch({ type: 'SET_LOADING'})
 
         const getUserData = async() => {
-            const userData = await getUser(params.login)
+            const userData = await getUserAndRepos(params.login)
             dispatch({
-                type: 'GET_USER',
+                type: 'GET_USER_AND_REPOS',
                 payload: userData
-            })
-
-            const userRepoData = await getUserRepos(params.login)
-            dispatch({
-                type: 'GET_REPOS',
-                payload: userRepoData
             })
         }
 
